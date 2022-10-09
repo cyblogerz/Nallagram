444279787113298cfee467f05a4dcf3489018c83
Pranav Ajay <pranavajay2002@gmail.com>
2022-10-09 16:18:59 +0530

    Refined code

---
+++ lib/models/user_model.dart
@@ -0,0 +1,17 @@
+class User {
+  final String name;
+  final String desc;
+  final String profileurl;
+  final int followers;
+  final int following;
+  final int posts;
+
+  User({
+    this.desc,
+    this.profileurl,
+    this.followers,
+    this.following,
+    this.posts,
+    this.name,
+  });
+}
